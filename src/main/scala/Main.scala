8feb6cae8f49ae218999b101eafebeb976561dd4
ssharjeela <sharjeel.ahmed@logicielservice.com>
2025-06-18 17:31:17 +0500

    ADDITION IN THE CODE

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,10 @@
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
+//import java.awt.Window
+
+case class Employee(Name: String, Age: Int, Job: String, Salary: java.lang.Integer, JoinDate: String, DeptId: Int)
+case class Department(DeptId: Int, DeptName: String)
 
 object DummySparkJob {
   def main(args: Array[String]): Unit = {
@@ -13,18 +18,31 @@ object DummySparkJob {
 
     // Sample data (could also read from a file)
     val dummyData = Seq(
-      ("Alice", 25, "Engineer"),
-      ("Bob", 30, "Data Scientist"),
-      ("Charlie", 35, "Manager"),
-      ("Diana", 28, "Analyst")
+      Employee("Alice", 25, "Engineer", 60000, "2020-01-15", 101),
+      Employee("Bob", 30, "Data Scientist", 75000, "2019-06-10", 102),
+      Employee("Charlie", 35, "Manager", 90000, "2015-03-12", 101),
+      Employee("Diana", 28, "Analyst", 58000, "2021-07-01", 103),
+      Employee("Eve", 45, "Manager", 95000, "2012-11-20", 101),
+      Employee("Frank", 23, "Intern", 30000, "2023-01-10", 104),
+      Employee("Grace", 29, "Engineer", 64000, "2018-05-15", 101),
+      Employee("Heidi", 32, "Data Scientist", null, "2020-09-30", 102)
     )
 
     // Create a DataFrame
-    val df: DataFrame = dummyData.toDF("Name", "Age", "Job")
+    val df: DataFrame = dummyData.toDF("Name", "Age", "Job", "Salary", "JoinDate", "DeptId")
+
+
+    val departments = Seq(
+      (101, "Engineering"),
+      (102, "Data"),
+      (103, "Finance"),
+      (104, "HR")
+    ).toDF("DeptId","DeptName")
 
     // Show the raw data
     println("=== Raw Data ===")
     df.show()
+    departments.show()
 
     // Transformation: Filter people older than 28
     val filteredDf = df.filter(col("Age") > 28)
@@ -41,6 +59,33 @@ object DummySparkJob {
 
     println("=== Average Age by Job ===")
     avgAgeByJob.show()
+
+    val olderAge = df.filter(col("Age") > 28)
+    println ("==+ older age > 25")
+    olderAge.show()
+
+    val joinWithDpt = df.join(departments, "DeptId")
+    joinWithDpt.show()
+
+    val avgSalary = df.agg(avg("Salary")).first().getDouble(0)
+    
+    val salaryFixed = df.na.fill(Map("Salary" -> avgSalary)) 
+    salaryFixed.show()
+    
+    joinWithDpt.select("Name","Age","DeptName","Job").show()
+
+    val win = Window.partitionBy("DeptId").orderBy($"Salary".desc)
+    val ranked = joinWithDpt.withColumn("Rank", rank().over(win))
+
+    println("=== Salary Rank Per Department ===")
+    ranked.select("Name","DeptName", "Salary", "Rank").show()
+    
+
+
+
+
+
+
 
     // Write output (uncomment to use)
     // transformedDf.write.parquet("output/dummy_output.parquet")
